6b0358891f86db3e6c615d5c3f9e4b8b5699494c
Lazizbek Shodmonov <lazizbekshodmonov.brbtech@gmail.com>
2025-09-17 18:04:28 +0500

    change plum service

--- src/modules/plum/plum.service.ts
+++ src/modules/plum/plum.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePlumDto } from './dto/create-plum.dto';
 import { UpdatePlumDto } from './dto/update-plum.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PlumService {
+  private isActive: boolean;
+  private isProduction: boolean;
+  constructor(private readonly configService: ConfigService) {
+    this.isActive = configService.get<boolean>('providers.plum.active') ?? false;
+    this.isProduction = configService.get<boolean>('providers.plum.production') ?? false;
+  }
   create(createPlumDto: CreatePlumDto) {
     return 'This action adds a new plum';
   }
